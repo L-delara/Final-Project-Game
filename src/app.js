1d738acbb37ac408aa8ba78a2acf933ffde07561
Ldelara <ligia.delara15@gmail.com>
2023-04-04 23:16:40 -0400

    function to generate random die #

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-// create a dice roller
+// create a dice roller - 6 sides, random number
+const rollDice = () => 1 + Math.floor(Math.random() * 6);
+
+//show your roll
+function showRoll(number) {
+  let rolledNumer = document.getElementById("dice-face");
+  rolledNumer.innerHTML = number;
+}
+
+// let rollMe =
+document.getElementById("roll").onclick = function () {
+  showRoll(rollDice());
+};
