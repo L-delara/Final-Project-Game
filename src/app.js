d78c499f32121a79d4f47cf22733cac5e0796e9e
Ldelara <ligia.delara15@gmail.com>
2023-04-06 23:36:00 -0400

    Limited to 2 players, fixed weird css

--- src/app.js
+++ src/app.js
@@ -14,21 +14,37 @@ document.getElementById("roll").onclick = function () {
 
 // have players choose an avatar (wizard, cat, archer) and display next to player and bottom at start line
 function selectPlayer(choice) {
+  if (pickedAvatar >= 2) {
+    return;
+  }
   //get image element
   let wizard = document.getElementById("wizard-avatar");
   let cat = document.getElementById("cat-avatar");
   let archer = document.getElementById("archer-avatar");
 
+  let avatarName = "Wizard!";
   //copy image
   let avatarPick = wizard;
   if (choice === "cat-avatar") {
     avatarPick = cat;
+    avatarName = "Black Cat!";
   }
   if (choice === "archer-avatar") {
     avatarPick = archer;
+    avatarName = "Archer!";
   }
+  pickedAvatar += 1;
 
+  let avatarId = "";
+  if (pickedAvatar === 1) {
+    avatarId = "avatar1";
+  }
+  if (pickedAvatar === 2) {
+    avatarId = "avatar2";
+  }
   //move to starting line
   let startLine = document.querySelector(".startLine");
   startLine.appendChild(avatarPick);
+
+  document.getElementById(avatarId).innerHTML = avatarName;
 }
