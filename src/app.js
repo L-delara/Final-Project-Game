00cc19db6d0e4bec5139092ca75924d3d44a3ed3
Ldelara <ligia.delara15@gmail.com>
2023-04-06 22:30:09 -0400

    players able to select avatar
    need to limit to two and return extra

--- src/app.js
+++ src/app.js
@@ -12,4 +12,23 @@ document.getElementById("roll").onclick = function () {
   showRoll(rollDice());
 };
 
-//add snakes and character avatars
+// have players choose an avatar (wizard, cat, archer) and display next to player and bottom at start line
+function selectPlayer(choice) {
+  //get image element
+  let wizard = document.getElementById("wizard-avatar");
+  let cat = document.getElementById("cat-avatar");
+  let archer = document.getElementById("archer-avatar");
+
+  //copy image
+  let avatarPick = wizard;
+  if (choice === "cat-avatar") {
+    avatarPick = cat;
+  }
+  if (choice === "archer-avatar") {
+    avatarPick = archer;
+  }
+
+  //move to starting line
+  let startLine = document.querySelector(".startLine");
+  startLine.appendChild(avatarPick);
+}
