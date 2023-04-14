e94f605fa8617b346c40d0da653e2885303bdc2a
Ldelara <ligia.delara15@gmail.com>
2023-04-14 00:42:14 -0400

    quit snakes, switched to dice
    Need to reformat css, a score-keeping system, and to fix disappearing avatars (???)

--- README.md
+++ README.md
@@ -10,17 +10,14 @@ logic:
 -- number randomizer for die rolls 1-6 (nailed it!)
 
 Next:
-
-- player selects character (user input, display on screen)
-
--- async/await - die roll result is the next move
--- climb/jump ahead spaces, fall back spaces ()
+Made life too hard with snakes and ladders. switched to something more doable in this time frame. Nearly there.
 
 Image Credits
 -- Characters:
 magician: <a href="https://www.flaticon.com/free-icons/wizard" title="wizard icons">Wizard icons created by max.icons - Flaticon</a>
 bow archer: <a href="https://www.flaticon.com/free-icons/rpg" title="rpg icons">Rpg icons created by max.icons - Flaticon</a>
 cat<a href="https://www.flaticon.com/free-icons/cat" title="cat icons">Cat icons created by amonrat rungreangfangsai - Flaticon</a>
+
 -- Snakes
 rattlesnake: <a href="https://www.flaticon.com/free-icons/rattlesnake" title="rattlesnake icons">Rattlesnake icons created by Freepik - Flaticon</a>
 baby snake:<a href="https://www.flaticon.com/free-icons/snake" title="snake icons">Snake icons created by Freepik - Flaticon</a>
---
+++ dice-sources/dice.js
@@ -0,0 +1,58 @@
+// create a dice roller - 6 sides, random number
+const rollDice = () => 1 + Math.floor(Math.random() * 6);
+
+//show your roll
+function showRoll1(number) {
+  let rolledNumber1 = document.getElementById("dice1");
+  rolledNumber1.innerHTML = number;
+}
+function showRoll2(number) {
+  let rolledNumber2 = document.getElementById("dice2");
+  rolledNumber2.innerHTML = number;
+}
+// let rollMe =
+document.getElementById("roll1").onclick = function () {
+  showRoll1(rollDice());
+};
+document.getElementById("roll2").onclick = function () {
+  showRoll2(rollDice());
+};
+//-----------------------------
+// have players choose an avatar (wizard, cat, archer) and display next to player and bottom at start line
+function selectPlayer(choice) {
+  if (pickedAvatar >= 2) {
+    return;
+  }
+  //get image element
+  let wizard = document.getElementById("wizard-avatar");
+  let cat = document.getElementById("cat-avatar");
+  let archer = document.getElementById("archer-avatar");
+
+  let avatarName = "Wizard!";
+  //copy image
+  let avatarPick = wizard;
+  if (choice === "cat-avatar") {
+    avatarPick = cat;
+    avatarName = "Black Cat!";
+  }
+  if (choice === "archer-avatar") {
+    avatarPick = archer;
+    avatarName = "Archer!";
+  }
+  pickedAvatar += 1;
+
+  let avatarId = "";
+  if (pickedAvatar === 1) {
+    avatarId = "avatar1";
+  }
+  if (pickedAvatar === 2) {
+    avatarId = "avatar2";
+  }
+  //move to starting line
+  let chosen1 = document.querySelector("#avatar1");
+  chosen1.appendChild(avatarPick);
+  let chosen2 = document.querySelector("#avatar2");
+  chosen2.appendChild(avatarPick);
+
+  document.getElementById(avatarId).innerHTML = avatarName;
+}
